c3b9f9e2b541246cf966daae98b61970c79f23f7
Felipe Rebeca <felipetomazrebeca192@gmail.com>
2018-10-02 10:55:52 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router'
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -16,14 +17,21 @@ import { ConsultaService } from 'src/app/consulta/consulta.service';
 import { WelcomeComponent } from './barra-navegacao/welcome/welcome.component';
 import { FaturamentoComponent } from './faturamento/faturamento.component';
 import { FaturamentoService } from 'src/app/faturamento/faturamento.service';
+import { TabelaClientesComponent } from './tabela-clientes/tabela-clientes.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from 'src/app/login/auth.service';
+import { LogoutComponent } from './logout/logout.component';
 
 
 let routes = [
+  { path : "login", component : LoginComponent },
+  { path : "logout", component : LogoutComponent },
   { path : "resumo", component : ResumoComponent },
   { path : "consulta", component : ConsultaComponent },
   { path : "welcome", component : WelcomeComponent },
   { path : "faturamento", component :FaturamentoComponent },
-  { path : "**", redirectTo:"/welcome" }
+  { path : "tabela-clientes", component :TabelaClientesComponent },
+  { path : "**", redirectTo:"/login" }
 ]
 
 
@@ -38,15 +46,18 @@ let routes = [
     ConsultaPorEspecialidadeComponent,
     WelcomeComponent,
     FaturamentoComponent,
-    
+    TabelaClientesComponent,
+    LoginComponent,
+    LogoutComponent,
      
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [ResumoService, ConsultaService, FaturamentoService],
+  providers: [ResumoService, ConsultaService, FaturamentoService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
